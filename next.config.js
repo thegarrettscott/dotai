57128a898ff29cf3d7d705beb3569446d414f87b
garrett-mccurrach <garrett.mccurrach@eagles.oc.edu>
2025-09-07 18:52:31 -0500

    Remove complex redirects, use simple setup

--- next.config.js
+++ next.config.js
@@ -3,15 +3,6 @@ const nextConfig = {
   images: {
     domains: ['localhost'],
   },
-  async redirects() {
-    return [
-      {
-        source: '/((?!browser|api|_next).*)',
-        destination: '/browser',
-        permanent: false,
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
