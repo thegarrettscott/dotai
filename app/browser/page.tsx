b5c8ee4d24754928f66b05507134ec4617def20d
garrett-mccurrach <garrett.mccurrach@eagles.oc.edu>
2025-09-07 18:55:10 -0500

    Make browser the index page - remove redirects

--- app/browser/page.tsx
+++
@@ -1,9 +0,0 @@
-import { WebBrowser } from '@/components/WebBrowser'
-
-export default function BrowserPage() {
-  return (
-    <div className="min-h-screen bg-white">
-      <WebBrowser />
-    </div>
-  )
-}
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,9 @@
-'use client'
-
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
+import { WebBrowser } from '@/components/WebBrowser'
 
 export default function Home() {
-  const router = useRouter()
-
-  useEffect(() => {
-    // Redirect to browser page
-    router.push('/browser')
-  }, [router])
-
   return (
-    <div className="min-h-screen bg-white flex items-center justify-center">
-      <div className="text-center">
-        <h1 className="text-2xl font-bold text-gray-900 mb-4">
-          This Internet Does Not Exist
-        </h1>
-        <p className="text-gray-600">Redirecting to AI Web Browser...</p>
-      </div>
+    <div className="min-h-screen bg-white">
+      <WebBrowser />
     </div>
   )
 }
